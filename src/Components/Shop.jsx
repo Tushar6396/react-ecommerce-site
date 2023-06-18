0a40e4b7e7680da1dc44adc8c4a3fb0a24f172fd
Tushar <111505346+Tushar6396@users.noreply.github.com>
2023-06-18 18:27:40 +0530

    displayed the products from the api

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import './Styles/Shop.css'
 
 function Shop() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+    .then((res) => setData(res.data))
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  },[])
+
   return (
-    <div>
-      Shop
-    </div>
+    <>
+      <h1 className='heading'>Products Available</h1>
+      <div className="products">
+        {data.map((post) => {
+          const { id, title, category, description, image, price } = post;
+          return <div className="card" key={id}>
+              <h2 className="category">{category.toUpperCase()}</h2>
+              <h3 className='title'>{title.toUpperCase().slice(1,30)}</h3>
+              <img src={image} alt="" className='image' 
+              style={{ maxWidth: '100%', maxHeight: '100px' }}/>
+              <p className="description">{description.slice(1,100)}</p>
+              <span className='price'>Price: {price}</span>
+            </div>  
+        })}
+      </div>
+    </>
   )
 }
 
